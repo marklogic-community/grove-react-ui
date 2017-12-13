daed4437961240106607aa28a484199ddfdc20b1
Patrick McElwee <pmcelwee@gmail.com>
2017-12-13 16:33:37 -0500

    Pass contentType to <MLDetailView>

--- client/src/containers/MLDetailContainer.js
+++ client/src/containers/MLDetailContainer.js
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { MLDetailView } from 'ml-treehouse-react';
 
-let MLDetailContainer = class MLDetailContainer extends Component {
-  render() {
-    return (
-      <div>
-        <MLDetailView {...this.props} />
-      </div>  
-    );
-  }
-};
-
 const mapStateToProps = (state, ownProps) => {
   const sel = ownProps.selectors;
   return {
-    detail: sel.documentByUri(state, ownProps.uri)
+    detail: sel.documentByUri(state, ownProps.uri),
+    contentType: sel.contentTypeByUri(state, ownProps.uri)
   };
 };
 
@@ -25,9 +15,9 @@ const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
   loadDetail: ownProps.actions.fetchDoc
 }, dispatch);
 
-MLDetailContainer = connect(
+const MLDetailContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(MLDetailContainer);
+)(MLDetailView);
 
 export default MLDetailContainer;
