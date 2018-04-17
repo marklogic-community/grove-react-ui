c1f4b6effcd4e8404a6bc1ba1f4d8e0717191a53
Patrick McElwee <pmcelwee@gmail.com>
2018-04-17 16:48:13 -0400

    Remove logo more effectively

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar as MLNavbar } from 'muir-react';
 
 const Navbar = props => (
-  <MLNavbar logo={'#'} title="MarkLogic UI Resources" {...props}>
+  <MLNavbar title="MarkLogic UI Resources" {...props}>
     <Nav>
       <LinkContainer exact to="/">
         <NavItem>Search</NavItem>
