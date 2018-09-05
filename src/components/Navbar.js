dffaac2209a3634ecd12faf2bb9f061453208130
Patrick McElwee <pmcelwee@gmail.com>
2018-09-05 16:43:09 -0400

    Updates to Navbar and muir-core libraries
    GROVE-348, GROVE-126, GROVE-307

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar as MLNavbar } from 'muir-core-react-components';
 
+const brandLink = props => <Link to="/" {...props} />;
 const Navbar = props => (
-  <MLNavbar title="MarkLogic UI Resources" {...props}>
+  <MLNavbar title="MarkLogic UI Resources" {...props} brandLink={brandLink}>
     <Nav>
       <LinkContainer exact to="/">
         <NavItem>Search</NavItem>
