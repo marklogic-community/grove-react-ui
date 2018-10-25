9753e51e90e76800bbb73989afa52290b03dfd13
Patrick McElwee <pmcelwee@gmail.com>
2018-10-25 15:32:24 -0400

    GROVE-428: Go to requested page after login or status-check

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,40 +8,77 @@ import {
   LoginContainer
 } from 'grove-core-react-redux-containers';
 
-const LoggedInRoutes = () => (
-  <Switch>
-    <Route exact path="/" render={() => <SearchContainer />} />
+const PrivateRoute = ({
+  component: Component,
+  render,
+  isAuthenticated,
+  ...rest
+}) => {
+  return (
     <Route
-      exact
-      path="/detail/:id*"
-      render={props => {
-        const pathParts = window.location.pathname.split('/');
-        // React Router tries to decode the id, but inconsistently
-        // It breaks when back-forward clicked in browser
-        return <DetailContainer id={pathParts[pathParts.length - 1]} />;
-      }}
+      {...rest}
+      render={props =>
+        isAuthenticated ? (
+          render ? (
+            render(props)
+          ) : (
+            <Component {...props} />
+          )
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
     />
-    <Route
-      exact
-      path="/create"
-      render={() => <CreateContainer redirectPath="/detail" />}
-    />
-    <Redirect from="/login" to="/" />
-  </Switch>
-);
+  );
+};
 
-const LoggedOutRoutes = props => (
-  <Switch>
-    <Route exact path="/login" component={LoginContainer} />
-    <Redirect exact to="/login" state={{ from: props.location }} />
-  </Switch>
-);
-
-const Routes = ({ isAuthenticated }, ...props) => {
-  return isAuthenticated ? (
-    <LoggedInRoutes {...props} />
-  ) : (
-    <LoggedOutRoutes {...props} />
+const Routes = ({ isAuthenticated }, ...rest) => {
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/login"
+        render={props => {
+          return isAuthenticated ? (
+            <Redirect
+              to={(props.location.state && props.location.state.from) || '/'}
+            />
+          ) : (
+            <LoginContainer />
+          );
+        }}
+      />
+      <PrivateRoute
+        isAuthenticated={isAuthenticated}
+        exact
+        path="/"
+        render={() => <SearchContainer />}
+      />
+      <PrivateRoute
+        isAuthenticated={isAuthenticated}
+        exact
+        path="/detail/:id*"
+        render={props => {
+          const pathParts = window.location.pathname.split('/');
+          // React Router tries to decode the id param, but inconsistently
+          // It breaks when back-forward clicked in browser
+          // See https://github.com/ReactTraining/history/issues/505
+          // for current status or recommended workarounds.
+          return <DetailContainer id={pathParts[pathParts.length - 1]} />;
+        }}
+      />
+      <PrivateRoute
+        isAuthenticated={isAuthenticated}
+        exact
+        path="/create"
+        render={() => <CreateContainer redirectPath="/detail" />}
+      />
+    </Switch>
   );
 };
 
